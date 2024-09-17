43d49fbae4513a8d6f0351f083fea8f2361b3ec9
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-09-17 23:33:18 +0530

    initial commit

---
+++ Test-01/product-service/src/main/java/edu/icet/crm/product/model/Product.java
@@ -0,0 +1,16 @@
+package edu.icet.crm.product.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class Product {
+
+    private String name;
+    private String description;
+    private String price;
+
+}
---
+++ Test-01/product-service/src/main/java/edu/icet/crm/product/service/ProductService.java
@@ -0,0 +1,7 @@
+package edu.icet.crm.product.service;
+
+import edu.icet.crm.product.model.Product;
+
+public interface ProductService {
+    void saveProduct(Product product);
+}
