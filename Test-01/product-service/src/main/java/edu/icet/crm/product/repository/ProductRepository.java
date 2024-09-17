7f5966114330aa4b893df8700b55c2527fc3931f
Sanjaya Samudra <178027994+Sanjaya-Samudra@users.noreply.github.com>
2024-09-18 00:30:05 +0530

    Delete Test-01/product-service directory

--- Test-01/product-service/src/main/java/edu/icet/crm/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.icet.crm.product.repository;
-
-import edu.icet.crm.product.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-
-}
--- Test-01/product-service/src/main/java/edu/icet/crm/product/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package edu.icet.crm.product.service;
-
-import edu.icet.crm.product.model.Product;
-
-public interface ProductService {
-    void saveProduct(Product product);
-}
